60765107f3775b3879f5bf555109a02da1e7ed38
Alexandre Stahmer <alexandre.stahmer@acseo-conseil.fr>
2019-12-07 01:32:43 +0100

    wip: rework useForm using ref instead of reducer

--- src/hooks/form/useForm.ts
+++ src/hooks/form/useForm.ts
@@ -1,23 +1,52 @@
-import { ChangeEvent, Reducer, useReducer } from "react";
+import { ChangeEvent, FormEvent, Reducer, useCallback, useMemo, useRef } from "react";
 
-export function useForm<Fields>(fields: Fields): FormReturn<Fields> {
-    const [state, dispatch] = useReducer(reducer, { ...initialState, fields });
+import { getProp } from "@/functions/utils";
+
+export function useForm<Values extends FormValues>(
+    data: Values,
+    validations?: FormState<Values>["validations"]
+): FormReturn<Values> {
+    const state = useRef({ ...initialState, data, validations });
+    const dispatch = useCallback((action: FormActionPayload) => reducer(state.current, action), []);
 
     const doAction = <T = FieldType>(type: FormActionType) => (name: string, value: T) =>
-        dispatch({ type, name, value });
+        (state.current = dispatch({ type, name, value }));
 
-    const set = doAction(FormActionType.SET);
-    const add = doAction(FormActionType.ADD);
-    const remove = doAction<number>(FormActionType.REMOVE);
-    const reset = () => dispatch({ type: FormActionType.RESET });
+    const actions = useMemo(
+        () => ({
+            // Getters
+            get: (path: string | string[]) =>
+                typeof path === "string"
+                    ? getProp(state.current.data, path)
+                    : path.map((pathItem) => getProp(state.current.data, pathItem)),
+            getValues: () => state.current.data,
+            getState: () => state.current,
+            // Values
+            set: doAction(FormActionType.SET),
+            add: doAction(FormActionType.ADD),
+            remove: doAction<number>(FormActionType.REMOVE),
+            reset: () => dispatch({ type: FormActionType.RESET, value: { data, validations } }),
+            // Error
+            addError: doAction(FormActionType.ADD_ERROR),
+            removeError: doAction<number>(FormActionType.REMOVE_ERROR),
+            // Events
+            onChange: (name: string) => (event: ChangeEvent<HTMLInputElement>) =>
+                doAction(FormActionType.SET)(name, event.target.value),
+            onSubmit: (callback: FormSubmitCallback<Values>) => (event: FormEvent) => {
+                if (event) {
+                    event.preventDefault();
+                    event.persist();
+                }
+                callback(state.current.data, event);
+            },
+        }),
+        []
+    );
 
-    const addError = doAction(FormActionType.ADD_ERROR);
-    const removeError = doAction<number>(FormActionType.REMOVE_ERROR);
-
-    const onChange = (name: string) => (event: ChangeEvent<HTMLInputElement>) => set(name, event.target.value);
-
-    return [state, { set, add, remove, reset, addError, removeError, onChange }];
+    return [state.current, actions];
 }
+
+export type FormValues = Record<string, any>;
 
 export enum FormActionType {
     SET,
@@ -31,47 +60,83 @@ export enum FormActionType {
 type FieldBasicType = string | number | boolean | object;
 type FieldType = FieldBasicType | Array<FieldBasicType>;
 
-// type ComplexField = { name: string; type: FieldType; validation?: Function; defaultValue?: FieldType };
-// type Field = string | ComplexField;
+type Validator<T> = (value: T) => boolean;
+type FieldsValidation<Values> = { [P in keyof Values]: Validator<Values[P]> };
 
-export type FormState<Fields = any> = { fields: Fields; errors: Record<string, string[]>; isValid: boolean };
-const initialState: FormState<any> = { fields: {}, errors: {}, isValid: undefined };
+export type FormState<Values = any> = {
+    data: Values;
+    validations: Partial<FieldsValidation<Values>>;
+    errors: Record<string, string[]>;
+    isValid: boolean;
+};
+const initialState: FormState<any> = { data: {}, validations: {}, errors: {}, isValid: undefined };
 
-export type FormActions = {
+export type FormSubmitCallback<Values extends FormValues> = (data: Values, event: FormEvent) => void;
+export type FormActions<Values extends FormValues> = {
+    // Getters
+    get: (path: string | string[]) => any;
+    getValues: () => Values;
+    getState: () => FormState<Values>;
+    // Values
     set: (name: string, value: FieldType) => void;
     add: (name: string, value: FieldType) => void;
     remove: (name: string, value: FieldType) => void;
     reset: () => void;
+    // Errors
     addError: (name: string, value: FieldType) => void;
     removeError: (name: string, value: FieldType) => void;
+    // Events
     onChange: (name: string) => (event: ChangeEvent<any>) => void;
+    onSubmit: (callback: FormSubmitCallback<Values>) => (event: FormEvent<Element>) => void;
 };
-export type FormReturn<Fields> = [FormState<Fields>, FormActions];
+export type FormReturn<Values extends FormValues> = [FormState<Values>, FormActions<Values>];
 
 type FormActionPayload = { type: FormActionType; name?: string; value?: any };
 
 const reducer: Reducer<FormState<any>, FormActionPayload> = (state, { type, name, value }) => {
+    const hasChanged = didValueChange(type);
+
     switch (type) {
-        case FormActionType.SET:
-            return { ...state, fields: { ...state.fields, ...{ [name]: value } } };
-
-        case FormActionType.ADD:
-            return { ...state, fields: { ...state.fields, ...{ [name]: state.fields[name].concat(value) } } };
-
-        case FormActionType.REMOVE:
-            if (value >= 0 && state.fields[name][value]) {
-                state.fields[name].splice(value, 1);
+        case FormActionType.SET: {
+            const data = { ...state.data, ...{ [name]: value } };
+            if (hasChanged) {
+                const validationResult = getValidationResult(state.validations, data);
+                state.isValid = validationResult.isValid;
             }
 
-            return { ...state, fields: { ...state.fields, ...{ [name]: state.fields[name] } } };
+            return { ...state, data: data };
+        }
+
+        case FormActionType.ADD: {
+            const data = { ...state.data, ...{ [name]: state.data[name].concat(value) } };
+            if (hasChanged) {
+                const validationResult = getValidationResult(state.validations, data);
+                state.isValid = validationResult.isValid;
+            }
+            return { ...state, data: data };
+        }
+
+        case FormActionType.REMOVE: {
+            if (value >= 0 && state.data[name][value]) {
+                state.data[name].splice(value, 1);
+            }
+
+            const data = { ...state.data, ...{ [name]: state.data[name] } };
+            if (hasChanged) {
+                const validationResult = getValidationResult(state.validations, data);
+                state.isValid = validationResult.isValid;
+            }
+
+            return { ...state, data: data };
+        }
 
         case FormActionType.RESET:
-            return initialState;
+            return { ...initialState, ...value };
 
         case FormActionType.ADD_ERROR:
             return { ...state, errors: { ...state.errors, ...{ [name]: state.errors[name].concat(value) } } };
 
-        case FormActionType.REMOVE:
+        case FormActionType.REMOVE_ERROR:
             if (value >= 0 && state.errors[name][value]) {
                 state.errors[name].splice(value, 1);
             }
@@ -81,4 +146,27 @@ const reducer: Reducer<FormState<any>, FormActionPayload> = (state, { type, name
         default:
             throw new Error("Action type is unknown");
     }
+};
+
+const changeActions = [FormActionType.SET, FormActionType.ADD, FormActionType.REMOVE];
+const didValueChange = (type: FormActionType) => changeActions.includes(type);
+const getValidationResult = <Values extends FormValues>(
+    validations: Partial<FieldsValidation<Values>>,
+    data: Values
+) => {
+    const validationResult: Pick<FormState<Values>, "errors" | "isValid"> = { errors: {}, isValid: true };
+    let errorCount = 0;
+
+    for (const key in validations) {
+        if (validations.hasOwnProperty(key)) {
+            errorCount += validations[key](data[key]) ? 0 : 1;
+            console.log(validations[key](data[key]));
+            // TODO add error when validator has a custom error message
+            // result.errors[key] = validations[key](fields[key])
+        }
+    }
+
+    validationResult.isValid = errorCount < 1;
+
+    return validationResult;
 };
