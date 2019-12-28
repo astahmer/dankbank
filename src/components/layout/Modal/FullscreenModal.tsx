56e139a73952b96081c6f8f83a84556bd1b365c5
Alexandre Stahmer <alexandre.stahmer@acseo-conseil.fr>
2019-12-28 17:46:10 +0100

    refacto: FullscreenModal with useTransition

--- src/components/layout/Modal/FullscreenModal.tsx
+++ src/components/layout/Modal/FullscreenModal.tsx
@@ -2,8 +2,62 @@ import {
     Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader,
     ModalOverlay, Text, useColorMode
 } from "@chakra-ui/core";
-import { ReactNode, useEffect, useState } from "react";
-import { Transition } from "react-spring/renderprops.cjs";
+import { ReactNode } from "react";
+import { animated, useTransition } from "react-spring";
+
+export function FullscreenModal({
+    isOpen,
+    close,
+    children,
+    title,
+    header,
+    body,
+    footer,
+    isAnimated = true, // TODO
+    withHeader = true,
+    withFooter = true,
+    withCloseBtn = true,
+}: ModalProps) {
+    const { colorMode } = useColorMode();
+    const color = colorMode === "dark" ? "gray.100" : "inherited";
+
+    const transitions = useTransition(isOpen, null, {
+        from: { opacity: 0, transform: "translate3d(-10px, 0, 0)" },
+        enter: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+        leave: { opacity: 0, transform: "translate3d(10px, 0, 0)" },
+    });
+
+    return (transitions.map(
+        ({ item, key, props }) =>
+            item && (
+                <Modal isOpen={true} onClose={close} isCentered scrollBehavior={"inside"} key={key}>
+                    <AnimatedModalOverlay style={props} />
+
+                    <AnimatedModalContent w="100vw" minW="100vw" h="100vh" maxH="100vh" noStyles style={props}>
+                        {withHeader && <ModalHeader>{header || <Text color={color}>{title}</Text>}</ModalHeader>}
+
+                        {withCloseBtn && <ModalCloseButton color={color} />}
+
+                        <ModalBody overflowY="auto" display="flex" paddingX="10px" mt={!header && !title && "10px"}>
+                            {body || children}
+                        </ModalBody>
+
+                        {withFooter && (
+                            <ModalFooter pt="5px" pb="10px">
+                                {footer || (
+                                    <Flex w="100%" justify="flex-end">
+                                        <Button variant="outline" mr={3} onClick={close}>
+                                            Close
+                                        </Button>
+                                    </Flex>
+                                )}
+                            </ModalFooter>
+                        )}
+                    </AnimatedModalContent>
+                </Modal>
+            )
+    ) as any) as JSX.Element;
+}
 
 type ModalProps = Optional<ChildrenProp> & {
     isOpen: boolean;
@@ -18,67 +72,5 @@ type ModalProps = Optional<ChildrenProp> & {
     withCloseBtn?: boolean;
 };
 
-export function FullscreenModal({
-    isOpen,
-    close,
-    children,
-    title,
-    header,
-    body,
-    footer,
-    isAnimated = true,
-    withHeader = true,
-    withFooter = true,
-    withCloseBtn = true,
-}: ModalProps) {
-    const [immediate, setImmediate] = useState(false);
-
-    const { colorMode } = useColorMode();
-    const color = colorMode === "dark" ? "gray.100" : "inherited";
-
-    useEffect(() => {
-        setImmediate(!isOpen);
-    }, [isOpen]);
-
-    const makeModal = (styles: any = {}) => (
-        <Modal isOpen={isOpen} onClose={close} isCentered scrollBehavior={"inside"}>
-            <ModalOverlay opacity={styles.opacity} />
-
-            <ModalContent w="100vw" minW="100vw" h="100vh" maxH="100vh" noStyles {...styles}>
-                {withHeader && <ModalHeader>{header || <Text color={color}>{title}</Text>}</ModalHeader>}
-
-                {withCloseBtn && <ModalCloseButton color={color} />}
-
-                <ModalBody overflowY="auto" display="flex" paddingX="10px" mt={!header && !title && "10px"}>
-                    {body || children}
-                </ModalBody>
-
-                {withFooter && (
-                    <ModalFooter pt="5px" pb="10px">
-                        {footer || (
-                            <Flex w="100%" justify="flex-end">
-                                <Button variant="outline" mr={3} onClick={close}>
-                                    Close
-                                </Button>
-                            </Flex>
-                        )}
-                    </ModalFooter>
-                )}
-            </ModalContent>
-        </Modal>
-    );
-
-    return isAnimated ? (
-        <Transition
-            items={isOpen}
-            from={{ opacity: 0, transform: "translate3d(-10px, 0, 0)" }}
-            enter={{ opacity: 1, transform: "translate3d(0, 0, 0)" }}
-            leave={{ opacity: 0, transform: "translate3d(10px, 0, 0)" }}
-            immediate={immediate}
-        >
-            {(show) => show && ((styles) => makeModal(styles))}
-        </Transition>
-    ) : (
-        makeModal()
-    );
-}
+const AnimatedModalContent = animated(ModalContent);
+const AnimatedModalOverlay = animated(ModalOverlay);
