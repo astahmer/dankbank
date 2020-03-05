62b52ac0f8faaf3d0b0f19a6ebbf624994d4c0c4
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-03-05 21:03:32 +0100

    feat: useCombinedRefsCallback

--- src/hooks/useCombinedRefs.ts
+++ src/hooks/useCombinedRefs.ts
@@ -1,4 +1,4 @@
-import { MutableRefObject, Ref, useEffect, useRef } from "react";
+import { MutableRefObject, Ref, useCallback, useEffect, useRef } from "react";
 
 export function useCombinedRefs<T = any>(...refs: Array<Ref<T> | MutableRefObject<T>>) {
     const targetRef = useRef<T>();
@@ -17,3 +17,25 @@ export function useCombinedRefs<T = any>(...refs: Array<Ref<T> | MutableRefObjec
 
     return targetRef;
 }
+
+export const useCombinedRefsCallback = <T extends any>(
+    ...refs: Array<Ref<T> | MutableRefObject<T>>
+): ((element: T) => void) =>
+    useCallback(
+        (element: T) =>
+            refs.forEach((ref) => {
+                if (!ref) {
+                    return;
+                }
+
+                // Ref can have two types - a function or an object. We treat each case.
+                if (typeof ref === "function") {
+                    return ref(element);
+                }
+
+                // As per https://github.com/facebook/react/issues/13029
+                // it should be fine to set current this way.
+                (ref as any).current = element;
+            }),
+        refs
+    );
